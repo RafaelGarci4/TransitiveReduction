9429e95e8ea17b5d652dce40f3548e038d24477b
Rafael Ferreira Garcia <rafa.ferreira888@gmail.com>
2023-06-07 19:52:35 -0300

    main do projeto
    versao pessoal

---
+++ transreduction.cpp
@@ -0,0 +1,25 @@
+#include <iostream>  // Biblioteca para entrada/saída padrão
+#include <vector>  // Biblioteca para vetores
+#include <unordered_map>  // Biblioteca para mapas não ordenados
+#include <unordered_set>  // Biblioteca para conjuntos não ordenados
+#include <queue>  // Biblioteca para filas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+
+    return 0;
+}
